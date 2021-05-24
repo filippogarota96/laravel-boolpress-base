9e0b0c22ad00379da0ed6b7928c5bfba4d185adb
Filippo Garota <filogarota101@gmail.com>
2021-05-24 17:22:09 +0200

    Aggiunti i commenti fake

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comment;
+use App\Post;
+use Faker\Generator as Faker;
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {   
+        //filtro solo i post pubblicati
+        $posts = Post::where('published', 1)->get();
+        //ciclo questi post
+        foreach($posts as $post)
+        {
+            //per ogni post vado a generare un numero random di commenti
+            for ($i=0; $i < rand(0, 5) ; $i++) { 
+                //creo l'istanza del nuovo commento
+                $newComment = new Comment();
+                $newComment->post_id = $post->id;
+                $newComment->name = $faker->name();
+                $newComment->content = $faker->text();
+                $newComment->save();
+            }
+        }
+    }
+}
