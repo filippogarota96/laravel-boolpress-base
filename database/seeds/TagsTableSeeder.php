037db29a1c74c42f3eeda7bb0cfbbf465818187a
Filippo Garota <filogarota101@gmail.com>
2021-05-26 17:43:29 +0200

    Tag seeders

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+class TagsTableSeeder extends Seeder
+
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //creo l'array con i seeds
+        $tags = [
+            'html',
+            'css',
+            'js',
+            'php',
+            'vuejs',
+            'laravel',
+            'sass'
+        ];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag->save();
+        }
+    }
+}
